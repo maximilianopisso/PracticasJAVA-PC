97b4ee117e9feb240589b2cf43aecf950a50dc48
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2021-11-03 13:19:48 -0300

    prueba2

--- Prueba2.java
+++ Prueba2.java
@@ -1,6 +1,8 @@
 public class Prueba2 {
     
     public static void main(String[] args) {
-        System.out.println("hola");;
+        System.out.println("hola");
+
+        System.out.println("hola2");
     }
 }
